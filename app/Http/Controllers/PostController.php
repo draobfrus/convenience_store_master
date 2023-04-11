619f412c1f099c2f229033662db97f74abf67596
draobfrus <aelfci5am@gmail.com>
2023-04-11 10:37:57 +0900

    [Change]postコントローラと対応するviewを変更

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view('posts.post');
+        $values = Post::all();
 
+        $count = Post::count(); // 数字
+
+        $first = Post::findOrFail(1); // インスタンス
+
+        $whereBBB = Post::where('text', '=', 'aaa')->get();
+
+        $queryBuilder = DB::table('posts')->where('text', '=', 'aaa')
+        ->select('id', 'text')
+        ->get();
+
+        // dd($values, $count, $first, $whereBBB, $queryBuilder);
+
+        return view('posts.post', compact('values'));
     }
 }
--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -1,1 +1,6 @@
-post
+post<br>
+
+@foreach($values as $value)
+{{ $value->id }} <br>
+{{ $value->text }} <br>
+@endforeach
