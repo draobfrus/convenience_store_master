5836a5c71ad0db936e5b404c6704da5ed494de25
draobfrus <aelfci5am@gmail.com>
2023-04-11 09:58:59 +0900

    [Add]postコントローラと対応するviewのテスト

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('posts/post', [PostController::class, 'index']); // 配列で書く
 
 Route::get('/dashboard', function () {
     return view('dashboard');
