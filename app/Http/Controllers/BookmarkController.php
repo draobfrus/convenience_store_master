9bc448529db0c56c875d95cd063f9be4faf8b450
draobfrus <aelfci5am@gmail.com>
2023-04-16 14:25:47 +0900

    [Add]BookmarkControllerにstore, destroyメソッドを追加し、対応するルーティングを追記

---
+++ app/Http/Controllers/BookmarkController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookmarkController extends Controller
+{
+    public function store($postId) {
+
+
+    }//
+
+    public function destroy($postId) {
+
+
+    }//
+}
