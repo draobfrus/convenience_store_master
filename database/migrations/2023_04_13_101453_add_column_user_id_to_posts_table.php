1d0225cd5d2f8dcda8681fef46cc3381db2cf80d
draobfrus <aelfci5am@gmail.com>
2023-04-16 10:30:06 +0900

    [Change]postsテーブルのuser_idに外部キー制約を追加し、再度migrate

--- database/migrations/2023_04_13_101453_add_column_user_id_to_posts_table.php
+++ database/migrations/2023_04_13_101453_add_column_user_id_to_posts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->foreignId('user_id')->after('image');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade')->after('image');
         });
     }
 
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table-dropColumn('user_id');//
+            $table->dropColumn('user_id');//
         });
     }
 };
