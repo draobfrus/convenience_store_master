c9f3060e4250458eb3d2bf4aa418b7bb78b03fe2
draobfrus <aelfci5am@gmail.com>
2023-04-16 14:42:40 +0900

    [Add]BookmarkControllerにブックマーク追加・解除のメソッドを作成

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -7,12 +7,18 @@ use Illuminate\Http\Request;
 class BookmarkController extends Controller
 {
     public function store($postId) {
-
-
+        $user = \Auth::user();
+        if (!$user->is_bookmark($postId)) {
+            $user->bookmark_posts()->attach($postId);
+        }
+        return back();
     }//
 
     public function destroy($postId) {
-
-
+        $user = \Auth::user();
+        if ($user->is_bookmark($postId)) {
+            $user->bookmark_posts()->detach($postId);
+        }
+        return back();
     }//
 }
